f520f735ce2c2954480bf27f5ff193c3fcf42872
nikita.stovpak@digitalsuits.co <stovpak1999@gmail.com>
2024-04-03 18:53:52 +0300

    fix: update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,9 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./Pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./Components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./App/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
